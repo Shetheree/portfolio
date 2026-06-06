3e307b19e137614bff3b184c80d5c179586fa0c3
Rajath <rajathshet.005@gmail.com>
2026-06-06 19:06:39 +0530

    Debig

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ import os
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
+app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 mail = Mail(app)
 
 @app.route('/')
